ef94504f0b5afc09963ffdc20d35dd66dc0f740a
Jimmy <66657082+JMDoza@users.noreply.github.com>
2024-09-18 07:06:43 -0400

    Added basic html and css to simulate a calculator then added number and clear button functionality

--- calculator.js
+++ calculator.js
@@ -1,29 +1,76 @@
-const add = function(a, b) {
-	return a + b
-};
+let operator
+let num1 = 0
+let num2 = 0
 
-const subtract = function(a, b) {
-	return a - b
-};
+const displayText = document.querySelector(".cal-display-text")
 
-const multiply = function(arr) {
-  let total = 0  
-	return arr.reduce((total, num) => total * num)
-};
 
-const divide = function(arr) {
-    let total = 0  
-      return arr.reduce((total, num) => total / num)
-  };
+function initializeButtonEvents () {
+  const buttons = document.querySelectorAll(".num-button")
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      displayText.textContent += button.textContent
+    })
+  })
 
-const power = function(a, b) {
-	return Math.pow(a, b)
-};
+  const ac = document.querySelector(".cal-clear-button")
+  ac.addEventListener("click", () => clearDisplay())
+}
 
-const factorial = function(a) {
-  if (a == 0) {
-    return 1
-  } else {
-    return a * factorial(a-1)
+function clearDisplay(){
+  displayText.textContent = ""
+  
+}
+
+function operate (operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      add(num1, num2)
+      break;
+
+    case "-":
+      subtract(num1, num2)
+      break;
+  
+    case "*":
+      multiply(num1, num2)
+      break;
+
+    case "/":
+      divide(num1, num2)
+      break;
+
+    default:
+      break;
   }
+}
+
+function add(a, b) {
+  return a + b
 };
+
+function subtract(a, b) {
+  return a - b
+};
+
+function multiply(arr) {
+  return arr.reduce((total, num) => total * num)
+};
+
+function divide(arr) {
+  return arr.reduce((total, num) => total / num)
+};
+
+initializeButtonEvents()
+
+// function power(a, b) {
+//   return Math.pow(a, b)
+// };
+
+// function factorial(a) {
+//   if (a == 0) {
+//     return 1
+//   } else {
+//     return a * factorial(a - 1)
+//   }
+// };
